50a2369107a2ea280cbc530b6b016b50027c0c8e
mass10 <mass10.github@gmail.com>
2018-12-09 22:27:34 +0900

    Update HomeController.scala

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -20,5 +21,24 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
+  }
+
+  //
+  // text/plain
+  //
+  def hello() = Action { implicit request: Request[AnyContent] =>
+    Ok("hello!")
+  }
+
+  //
+  // application/json
+  //
+  def hello_json() = Action { implicit request: Request[AnyContent] =>
+  var locations = (
+    Map("address" -> "135-0033 東京都江東区深川 99-99-99", "email" -> "jimi.hendrix@docomo.ne.jp"),
+      Map("address" -> "603-8467 京都府京都市北区鷹峯南鷹峯町 999-999", "email" -> "jimi.hendrix@docomo.ne.jp"),
+    )
+    val json = Json.toJson(locations)
+    Ok(json)
   }
 }
